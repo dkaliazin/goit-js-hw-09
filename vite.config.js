eef5ab563b819b011a33c64deee293ee6ab0454f
dkaliazin <dkaliazin@gmail.com>
2024-05-26 02:19:46 +0300

    123

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,30 @@
 import { defineConfig } from 'vite';
+import glob from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig({
-  base: '/goit-js-hw-09/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/index.html',
-        gallery:'src/1-gallery.html',
-        form: 'src/2-form.html'
-      }
-    }
-  }
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+        },
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
